baf58b3f6e11a56b7648abbcafbe095ea0bb7408
Сергей Парахин <sergei.parakhin@ngst.ru>
2024-05-07 23:31:52 +1000

    Using spring boot

---
+++ src/main/java/ru/psy/kataHibernateMVC/kataHibernateMVC.java
@@ -0,0 +1,13 @@
+package ru.psy.kataHibernateMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class kataHibernateMVC {
+
+    public static void main(String[] args) {
+        SpringApplication.run(kataHibernateMVC.class, args);
+    }
+
+}
---
+++ src/test/java/ru/psy/kataHibernateMVC/kataHibernateMVCApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.psy.kataHibernateMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class kataHibernateMVCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
